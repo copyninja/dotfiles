078d82ceef71adcdd9671f3f48a98b4331f5b6d9
Vasudev Kamath <kamathvasudev@gmail.com>
2013-12-10 20:29:25 +0530

    new script for upgrading elpa packages which can be run from command line using
         emacs --batch --script ~/.emacs.d/upgrade-elpa-packages.el

---
+++ .emacs.d/upgrade-elpa-packages.el
@@ -0,0 +1,19 @@
+(setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
+                          ("gnu" . "http://elpa.gnu.org/packages/")
+                          ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+                          ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(package-initialize)
+
+;; refresh package list
+(package-refresh-contents)
+
+;; change to package-menu mode
+(package-list-packages)
+
+;; mark upgrades
+(package-menu-mark-upgrades)
+
+;; execute the action
+(package-menu-execute)
+
