af0730f1861db6311082c4c519fb0f1ac50faa21
Vasudev Kamath <kamathvasudev@gmail.com>
2014-11-23 17:22:44 +0530

    Re-enable marmalade repo

--- .emacs.d/upgrade-elpa-packages.el
+++ .emacs.d/upgrade-elpa-packages.el
@@ -1,6 +1,6 @@
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
                           ("gnu" . "http://elpa.gnu.org/packages/")
-                          ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+                          ("marmalade" . "http://marmalade-repo.org/packages/")
                           ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (package-initialize)
