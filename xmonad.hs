cd26b449add53f3b9d194a0c5e49c004d67acd11
Vasudeva Kamath <kamathvasudev@gmail.com>
2015-01-13 17:40:51 +0530

    Fix xmonad configuration
    Fix shifting and float value

--- xmonad.hs
+++ xmonad.hs
@@ -6,19 +6,24 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 myManageHooks = composeAll
-                [ className =? "URxvt" --> doShift "term"
-                , className =? "Iceweasel" --> doShift "browse"
-                , className =? "Emacs" --> doShift "dev"
+                [ className =? "URxvt" --> doShift "1:term"
+                , className =? "Iceweasel" --> doShift "2:browse"
+                , className =? "Emacs" --> doShift "4:dev"
+                , className =? "Quasselclient" --> doFloat
+                , className =? "Quasselclient" --> doShift "3:im"
+                , className =? "Skype" --> doFloat
+                , className =? "Skype" --> doShift "3:im"
                 , manageDocks
                  
                 ]
-
+main :: IO()
 main = do
      xmproc <- spawnPipe "/usr/bin/xmobar /home/vasudev/.xmobarrc"
      xmonad $ defaultConfig
          { workspaces = ["1:term", "2:browse", "3:im", "4:dev", "5", "6:video",
                                      "7", "8", "9", "0", "-", "="]
-         , manageHook = manageDocks <+> manageHook defaultConfig
+         , manageHook = manageDocks <+> myManageHooks
+                        <+> manageHook defaultConfig
          , layoutHook = avoidStruts $ layoutHook defaultConfig
          , logHook = dynamicLogWithPP xmobarPP
                      {
