2e294856e9ba7ab7883e04c58ff852c9674f1d62
Vasudeva Kamath <kamathvasudev@gmail.com>
2015-01-13 16:15:23 +0530

    Major code refactoring
    Rewrote init.el, inspired from @vu3rdd's. Drop unnecessary packages from
    installation and tangle org file into different .el files. Previous
    configuration used to crash emacs in daemon mode trying to run
    org-babel-load-file when the .el file existed.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,84 +1,87 @@
-;; Copyright: (c) 2013, by copyninja
+;; No cursors please
+(blink-cursor-mode 0)
+(show-paren-mode 1)
 
-;; Emacs24 specific package archive config
-;; setting this up first so that I can download proper packages from
-;; the archive
-(setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
-                          ("gnu" . "http://elpa.gnu.org/packages/")
-                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                          ("melpa" . "http://melpa.milkbox.net/packages/")))
+;; Yes or no replaced by y or n
+(defalias 'yes-or-no-p 'y-or-n-p)
 
-;; First initialize ELPA
+;; Don't want these
+(setq make-backup-files nil)
+(setq initial-scratch-message "")
+(setq inhibit-startup-message t)
+
+;; column number mode
+(column-number-mode 1)
+
+;; Don't hurt my ears
+(setq visibile-bell t)
+
+;; This is bound to f11 in Emacs 24.4
+;; (toggle-frame-fullscreen)
+(custom-set-variables
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
+;; Who use the bar to scroll?
+(progn
+  (scroll-bar-mode 0)
+  (tool-bar-mode 0)
+  (menu-bar-mode 0))
+
+;; my packages
+(require 'package)
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives
+	     '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("stable-melpa" . "http://stable.melpa.org/packages/"))
+;; (add-to-list 'package-archives
+;; 	     '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
+
 (package-initialize)
 
-;; See if the archive is already read else hit the archives
-(unless package-activated-list
+(unless package-archive-contents
   (package-refresh-contents))
 
-;; Packages that I need
-(setq packages-list
-      (append
-       '(
-         auto-complete
-         magit
-         rainbow-mode
-         elpy
-	 pep8
-         yasnippet
-	 dropdown-list
-         solarized-theme
-         inkpot-theme
-         zenburn-theme
-         zen-and-art-theme
-	 ample-theme
-	 monokai-theme
-         popup
-         php-mode
-         highlight-80+
-         dired+
-         scratch
-         rfringe
-         multi-term
-         notify
-         undo-tree
-	 rainbow-delimiters
-	 go-mode
-	 go-autocomplete
-	 go-eldoc
-	 go-errcheck
-	 go-play
-	 go-snippets
-	 flycheck
-	 smartparens
-	 emms
-	 hy-mode
-	 ix
-	 geiser
-	 ac-geiser
-	 clojure-mode
-	 nrepl
-	 powerline
-	 rw-hunspell
-	 rw-ispell
-	 rw-language-and-country-codes
-	 scss-mode
-	 haskell-mode
-	 ghc
-	 w3m
-	 debian-changelog-mode
-	 debpaste)
-       ))
+(setq package-list '(auto-complete magit rainbow-mode rainbow-delimiters elpy
+				   monokai-theme ample-theme zenburn-theme popup
+				   go-mode rust-mode haskell-mode ghc debian-changelog-mode
+				   debpaste rw-hunspell rw-ispell rw-language-and-country-codes w3m
+				   flycheck smartparens debbugs ox-reveal dropdown-list slime
+				   smtpmail-multi emms yasnippet))
+;; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
 
-;; Install each package from ELPA/MELPA/Tromey etc.
-(dolist (package packages-list)
-  (if (not (member package package-activated-list))
-        (package-install package)
-    (message "%s is already installed" package)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(debian-bug-from-address "Vasudev Kamath <kamathvasudev@gmail.com>")
+ '(debian-bug-download-directory "~/.emacs.d/debbugs/")
+ '(debian-bug-helper-program (quote reporbug))
+ '(user-full-name "Vasudev Kamath")
+ '(user-mail-address "vasudev@copyninja.info")
+ '(custom-safe-themes t))
+
+;; Load theme
+(load-theme 'monokai)
 
 ;; load up Org-mode and Org-babel
 (require 'org-install)
 (require 'ob) 
 (require 'ob-tangle)
 (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
-;; load up all literate org-mode files in this directory
-(mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
+(mapc #'org-babel-tangle-file (directory-files dotfiles-dir t "\\.org$"))
+
+(require 'cl-lib)
+(mapc #'load-file
+      (cl-remove-if
+       (lambda (file)
+	 (or (string-match-p "\\init.el$" file)
+	     (string-match-p "\\upgrade-elpa-packages.el$" file)))
+       (directory-files dotfiles-dir t "\\.el$")))
