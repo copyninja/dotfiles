1d6da7270a16114712453f0a24516ab7f6499efd
Vasudev Kamath <kamathvasudev@gmail.com>
2015-12-20 14:54:44 +0530

    Drop the Faces from here, its actually set in org file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -87,9 +87,4 @@
 	 (or (string-match-p "\\init.el$" file)
 	     (string-match-p "\\upgrade-elpa-packages.el$" file)))
        (directory-files dotfiles-dir t "\\.el$")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fantasque Sans Mono" :foundry "unknown" :slant italic :height 120 :width normal :weight normal)))))
+
